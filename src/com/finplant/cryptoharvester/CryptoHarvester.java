70fd428b3118a8db87aebedbfe4216f4a31fb94d
Aleksey Starodubcev <starodubcevs@inbox.lv>
2018-10-02 16:08:42 +0300

    Added mavencentral as a repository

--- src/com/finplant/cryptoharvester/CryptoHarvester.java
+++ src/com/finplant/cryptoharvester/CryptoHarvester.java
@@ -1,10 +1,20 @@
 package com.finplant.cryptoharvester;
 
+import info.bitrich.xchangestream.core.*;
+import info.bitrich.xchangestream.binance.*;
+import info.bitrich.xchangestream.poloniex.*;
+
 public class CryptoHarvester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		StreamingExchange exchange = 
+				StreamingExchangeFactory.INSTANCE.createExchange(PoloniexStreamingExchange.class.getName());
 
+		// Connect to the Exchange WebSocket API. Blocking wait for the connection.
+		exchange.connect().blockingAwait();
+
+		// Disconnect from exchange (non-blocking)
+		exchange.disconnect().subscribe(() -> LOG.info("Disconnected from the Exchange"));
+		System.out.print("Lorem Ipsum");
 	}
-
 }
