7aaa605e675954f7c1c1d68713a51e51688ef3aa
Aleksey Starodubcev <starodubcevs@inbox.lv>
2018-10-02 16:59:52 +0300

    Project setup done, no errors

--- src/main/java/com/finplant/cryptoharvester/CryptoHarvester.java
+++ src/main/java/com/finplant/cryptoharvester/CryptoHarvester.java
@@ -3,9 +3,12 @@ package com.finplant.cryptoharvester;
 import info.bitrich.xchangestream.core.*;
 import info.bitrich.xchangestream.binance.*;
 import info.bitrich.xchangestream.poloniex.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CryptoHarvester {
-
+	private static final Logger LOG = LoggerFactory.getLogger(CryptoHarvester.class);
+	
 	public static void main(String[] args) {
 		StreamingExchange exchange = 
 				StreamingExchangeFactory.INSTANCE.createExchange(PoloniexStreamingExchange.class.getName());
