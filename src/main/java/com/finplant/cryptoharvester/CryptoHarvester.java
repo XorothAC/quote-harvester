2e9bc39c6bf98b982626f11ae4319dde7b6efa1b
Aleksey Starodubcev <starodubcevs@inbox.lv>
2018-10-04 16:13:40 +0300

    RxJava attempts

--- src/main/java/com/finplant/cryptoharvester/CryptoHarvester.java
+++ src/main/java/com/finplant/cryptoharvester/CryptoHarvester.java
@@ -3,6 +3,8 @@ package com.finplant.cryptoharvester;
 import info.bitrich.xchangestream.core.*;
 import info.bitrich.xchangestream.binance.*;
 import info.bitrich.xchangestream.poloniex.*;
+
+import org.knowm.xchange.currency.CurrencyPair;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,14 +12,19 @@ public class CryptoHarvester {
 	private static final Logger LOG = LoggerFactory.getLogger(CryptoHarvester.class);
 	
 	public static void main(String[] args) {
+		subscribeToExchanges();
+		System.out.print("Lorem Ipsum");
+	}
+	
+	public static void subscribeToExchanges() {
+		// Poloniex streaming exchange
 		StreamingExchange exchange = 
 				StreamingExchangeFactory.INSTANCE.createExchange(PoloniexStreamingExchange.class.getName());
 
 		// Connect to the Exchange WebSocket API. Blocking wait for the connection.
 		exchange.connect().blockingAwait();
-
+		
 		// Disconnect from exchange (non-blocking)
 		exchange.disconnect().subscribe(() -> LOG.info("Disconnected from the Exchange"));
-		System.out.print("Lorem Ipsum");
 	}
 }
