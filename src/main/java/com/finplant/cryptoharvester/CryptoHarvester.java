1b4ecc9a4fec59f78f2dbc971a60bd559ee07474
Aleksey Starodubcev <starodubcevs@inbox.lv>
2018-10-05 15:44:44 +0300

    Added exchange subscription

--- src/main/java/com/finplant/cryptoharvester/CryptoHarvester.java
+++ src/main/java/com/finplant/cryptoharvester/CryptoHarvester.java
@@ -2,7 +2,7 @@ package com.finplant.cryptoharvester;
 
 import info.bitrich.xchangestream.core.*;
 import info.bitrich.xchangestream.binance.*;
-import info.bitrich.xchangestream.poloniex.*;
+import info.bitrich.xchangestream.poloniex2.*;
 
 import org.knowm.xchange.currency.CurrencyPair;
 import org.slf4j.Logger;
@@ -12,19 +12,32 @@ public class CryptoHarvester {
 	private static final Logger LOG = LoggerFactory.getLogger(CryptoHarvester.class);
 	
 	public static void main(String[] args) {
-		subscribeToExchanges();
-		System.out.print("Lorem Ipsum");
-	}
-	
-	public static void subscribeToExchanges() {
-		// Poloniex streaming exchange
-		StreamingExchange exchange = 
+		StreamingExchange binanceExchange = 
+				StreamingExchangeFactory.INSTANCE.createExchange(BinanceStreamingExchange.class.getName());
+
+		StreamingExchange poloniexExchange = 
 				StreamingExchangeFactory.INSTANCE.createExchange(PoloniexStreamingExchange.class.getName());
 
+		subscribeToExchange(binanceExchange);
+		subscribeToExchange(poloniexExchange);
+		LOG.info("Lorem Ipsum");
+	}
+	
+	public static void subscribeToExchange(StreamingExchange exchange) {
+		// Specify subscription objects
+		ProductSubscription subscription = 
+				ProductSubscription.create().addTicker(CurrencyPair.BTC_USDT).build();
+		
 		// Connect to the Exchange WebSocket API. Blocking wait for the connection.
-		exchange.connect().blockingAwait();
+		exchange.connect(subscription).blockingAwait();
+		LOG.info("Connected to exchange: "+exchange.toString());
+		
+		// Log ticker info for now
+		exchange.getStreamingMarketDataService().getTicker(CurrencyPair.BTC_USDT)
+			.subscribe(ticker -> {LOG.info(exchange.toString() + ": {}", ticker);},
+					throwable -> LOG.error("ERROR in getting trades: ", throwable));
 		
 		// Disconnect from exchange (non-blocking)
-		exchange.disconnect().subscribe(() -> LOG.info("Disconnected from the Exchange"));
+		//exchange.disconnect().subscribe(() -> LOG.info("Disconnected from the Exchange"));
 	}
 }
